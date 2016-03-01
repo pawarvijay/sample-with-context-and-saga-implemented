3acd659f728ed8427e8405d3a044c2a1c23d2489
vijay pawar <vijay777pawar@gmail.com>
2016-03-01 08:22:57 +0530

    Update businessRule.js

--- domain/lib/sales/accounting/businessRules/businessRule.js
+++ domain/lib/sales/accounting/businessRules/businessRule.js
@@ -1,7 +1,7 @@
 module.exports = require('cqrs-domain').defineBusinessRule({
   name: 'checkForError'
 }, function (changed, previous, events, command) {
-  if (changed.get('text1234').toLowerCase().indexOf('error') >= 0) {
+  if (changed.get('textDOES_NOT_EXIST').toLowerCase().indexOf('error') >= 0) {
     throw new Error('This is just a sample rule!');
   }
 });
